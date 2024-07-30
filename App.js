80b9689361abf797fc620aeb87ff5b47c63fd832
jxtsammy <robertflames001@gmail.com>
2024-07-30 10:33:01 +0000

    Update

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Demo from './app/AppDemo'
 import Login from './app/Options/Login'
 
 const App = () => {
   return (
     <view>
-      <Demo />
       <Login />
     </view>
   )
