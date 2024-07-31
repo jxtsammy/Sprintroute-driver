2cbd9dcef7c63a20458df272c642111f4eff85de
jxtsammy <robertflames001@gmail.com>
2024-07-31 23:13:37 +0000

    Update

--- index.js
+++ index.js
@@ -3,3 +3,41 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 registerRootComponent(App);
+const express = require('express')
+const cors=require('cors')
+const Driver = require('./config')
+const app=express()
+app.use(express.json())
+app.use(cors());
+
+
+app.get("/",async (req,res)=>{
+  const snapshot=await Driver.get()
+  const list = snapshot.docs.map((doc)=> ({ id:doc.id, ...doc.data()}))
+  res.send(list)
+});
+
+
+app.post("/create", async(req,res) => {
+  const data=req.body
+  await Driver.add({data})
+  res.send({msg:"User Added"})
+});
+
+app.post("/update", async(req,res) => {
+  const id=req.body.id
+  delete req.body.id
+  const data = req.body
+  await Driver.doc(id).update(data)
+  res.send({msg:"Updated"})
+});
+
+
+app.post("/delete", async(req,res) => {
+  const id=req.body.id
+  await Driver.doc(id).delete()
+  res.send({msg:"Deleted"})
+});
+
+
+app.listen(4000,()=>console.log("Up and Running"))
